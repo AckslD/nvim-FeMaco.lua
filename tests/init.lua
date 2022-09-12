b7eccd94b6632a71712c55c2091a0c8c43a5ecd9
Axel Dahlberg <git@valleymnt.com>
2022-09-12 13:09:52 +0200

    ci(): fix install queries

--- tests/init.lua
+++ tests/init.lua
@@ -2,8 +2,4 @@ vim.o.runtimepath = vim.o.runtimepath .. ',./rtps/plenary.nvim'
 vim.o.runtimepath = vim.o.runtimepath .. ',./rtps/nvim-treesitter'
 vim.o.runtimepath = vim.o.runtimepath .. ',.'
 
-_G.assert_equal_tables = function(tbl1, tbl2)
-    assert(vim.deep_equal(tbl1, tbl2), string.format("%s ~= %s", vim.inspect(tbl1), vim.inspect(tbl2)))
-end
-
 require('nvim-treesitter').setup()
